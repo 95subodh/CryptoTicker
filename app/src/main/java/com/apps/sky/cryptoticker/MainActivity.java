a8f44cb54c260d83aed7f96edf5af5e6fdd39727
95subodh <subodh29031995@gmail.com>
2017-09-11 00:41:21 +0530

    added the api to fetch data

--- app/src/main/java/com/apps/sky/cryptoticker/MainActivity.java
+++ app/src/main/java/com/apps/sky/cryptoticker/MainActivity.java
@@ -1,7 +1,10 @@
 package com.apps.sky.cryptoticker;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +12,14 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void sendMessage(View view)
+    {
+        EditText editText = (EditText) findViewById(R.id.edittext_id);
+
+        Intent intent = new Intent(MainActivity.this, ApiTestActivity.class);
+        intent.putExtra("crypto",editText.getText().toString().toLowerCase());
+        startActivity(intent);
     }
 }
