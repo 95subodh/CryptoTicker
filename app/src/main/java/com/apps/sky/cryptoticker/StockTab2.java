76eb211b34c9dcd95ee4314c1e899ee68e5e8953
95subodh <subodh29031995@gmail.com>
2017-09-23 15:04:45 +0530

    Revert "Revert "minor ui changes: added comma seperated numbers, converted updated time (though buggy)""
    This reverts commit 7d134fd404ce69c227ccb0c92499d3a7e9a2185c.

--- app/src/main/java/com/apps/sky/cryptoticker/StockTab2.java
+++ app/src/main/java/com/apps/sky/cryptoticker/StockTab2.java
@@ -81,7 +81,6 @@ public class StockTab2 extends Fragment {
                     linkToImage = obj.getString("urlToImage");
                 }
                     return finalJson;
-//                return name+" - "+price;
 
             } catch (MalformedURLException e) {
                 e.printStackTrace();
