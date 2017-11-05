a980becd2d627998ecde7d3ec3ccfd5a2b03bbbd
Ankita1809 <ankitaverma@ANKITAs-MacBook.local>
2017-11-05 17:34:46 +0530

    basic working ui added

--- app/src/main/java/com/apps/sky/cryptoticker/HomePage/HomePageTabs/MoreTab/Settings/ChangeCurrencyActivity.java
+++ app/src/main/java/com/apps/sky/cryptoticker/HomePage/HomePageTabs/MoreTab/Settings/ChangeCurrencyActivity.java
@@ -6,8 +6,10 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.AdapterView;
-import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.ListView;
+import android.widget.RadioButton;
+import android.widget.SimpleAdapter;
 import android.widget.TextView;
 
 import com.apps.sky.cryptoticker.Global.Constants;
@@ -15,12 +17,13 @@ import com.apps.sky.cryptoticker.Global.MyGlobalsFunctions;
 import com.apps.sky.cryptoticker.R;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 
 public class ChangeCurrencyActivity extends AppCompatActivity {
 
     String currency;
     ListView listView;
-    ArrayList<String> currencies = new ArrayList<>();
+    ArrayList<HashMap<String, Object>> currencies;
     MyGlobalsFunctions myGlobalsFunctions;
     SharedPreferences sharedPreferences;
 
@@ -30,28 +33,62 @@ public class ChangeCurrencyActivity extends AppCompatActivity {
         setContentView(R.layout.activity_change_currency);
         sharedPreferences = this.getSharedPreferences("com.apps.sky.cryptoticker", Context.MODE_PRIVATE);
 
-        currencies.add("INR");
-        currencies.add("USD");
-
         myGlobalsFunctions = new MyGlobalsFunctions(this);
-        TextView currentCurrencyTextView = findViewById(R.id.default_currency_value);
+        final TextView currentCurrencyTextView = findViewById(R.id.default_currency_value);
+        Button setCurrencyBtn = findViewById(R.id.set_currency_btn);
         currency = sharedPreferences.getString(Constants.CURRENT_CURRENCY, new String());
         if (currency.equals("")) currency = "INR";
         currentCurrencyTextView.setText(currency);
 
-        listView = findViewById(R.id.choose_currency_listview);
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(ChangeCurrencyActivity.this,
-                R.layout.search_list_view_item, currencies);
-        listView.setAdapter(adapter);
+        currencies = new ArrayList<>();
+        HashMap<String, Object> map1 = new HashMap<>();
+        map1.put("currency", "INR");
+        currencies.add(map1);
+        HashMap<String, Object> map2 = new HashMap<>();
+        map2.put("currency", "USD");
+        currencies.add(map2);
+        HashMap<String, Object> map3 = new HashMap<>();
+        map3.put("currency", "EUR");
+        currencies.add(map3);
 
+        for (HashMap<String, Object> m :currencies)
+            m.put("checked", false);
+        listView = findViewById(R.id.choose_currency_listview);
+        listView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
+
+        final SimpleAdapter adapter = new SimpleAdapter(this, currencies, R.layout.radio_btn_listview_item, new String[] {"currency", "checked"}, new int[] {R.id.currency_text, R.id.choose_btn});
+        listView.setAdapter(adapter);
         listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
             @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                String temp = listView.getItemAtPosition(i).toString();
-                sharedPreferences.edit().putString(Constants.CURRENT_CURRENCY, temp).apply();
+            public void onItemClick(AdapterView<?> arg0, View v, int arg2, long arg3) {
+                RadioButton rb = v.findViewById(R.id.choose_btn);
+                if (!rb.isChecked())
+                {
+                    for (HashMap<String, Object> curr :currencies)
+                        curr.put("checked", false);
+
+                    currencies.get(arg2).put("checked", true);
+                    adapter.notifyDataSetChanged();
+                }
             }
         });
+
+        setCurrencyBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (HashMap<String, Object> curr: currencies) {
+                    Boolean check = (Boolean) curr.get("checked");
+                    if (check) {
+                        String temp = (String) curr.get("currency");
+                        sharedPreferences.edit().putString(Constants.CURRENT_CURRENCY, temp).apply();
+                        currency = temp;
+                        currentCurrencyTextView.setText(temp);
+                        break;
+                    }
+                }
+            }
+        });
+
     }
-
-
 }
+
