80e2429387589722e53797052f40f0aeeb206a5f
Ankita1809 <ankitaverma@ANKITAs-MacBook.local>
2017-12-07 13:29:21 +0530

    separate api calls for every graph tab

--- app/src/main/java/com/apps/sky/cryptoticker/StockPage/StockInfoTab/StockInfoTab.java
+++ app/src/main/java/com/apps/sky/cryptoticker/StockPage/StockInfoTab/StockInfoTab.java
@@ -43,6 +43,7 @@ public class StockInfoTab extends Fragment implements View.OnClickListener {
     ValueLineSeries series24, series7, series14, series30, series60, series90, seriesAll;
     ArrayList<TextView> texts;
     ArrayList<Integer> views;
+    String currentSeries;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -111,7 +112,8 @@ public class StockInfoTab extends Fragment implements View.OnClickListener {
         seriesAll.setColor(getContext().getResources().getColor(R.color.colorPrimary));
         if (myGlobalsFunctions.isNetworkConnected()) {
             new JSONTask().execute(url, iconUrl);
-            new JSONTaskGraph().execute(highLowUrl24, highLowUrl7, highLowUrl14, highLowUrl30, highLowUrl60, highLowUrl90, highLowUrlAll);
+            new JSONTaskHighLow().execute(highLowUrl24);
+            currentSeries = "1";
         }
         return rootView;
     }
@@ -130,51 +132,100 @@ public class StockInfoTab extends Fragment implements View.OnClickListener {
             }
         }
 
+        cubicValueLineChart.clearChart();
+
         switch (view.getId()) {
             case (R.id.h_24):
-                cubicValueLineChart.clearChart();
-                cubicValueLineChart.addSeries(series24);
+                currentSeries = "1";
+                if (series24.getSeries().size() == 0)
+                    new JSONTaskGraph().execute(highLowUrl24, "1");
+                else
+                    cubicValueLineChart.addSeries(series24);
                 break;
 
             case (R.id.d_7):
-                cubicValueLineChart.clearChart();
-                cubicValueLineChart.addSeries(series7);
+                currentSeries = "2";
+                if (series7.getSeries().size() == 0)
+                    new JSONTaskGraph().execute(highLowUrl7, "2");
+                else
+                    cubicValueLineChart.addSeries(series7);
                 break;
 
             case (R.id.d_14):
-                cubicValueLineChart.clearChart();
-                cubicValueLineChart.addSeries(series14);
+                currentSeries = "3";
+                if (series14.getSeries().size() == 0)
+                    new JSONTaskGraph().execute(highLowUrl14, "3");
+                else
+                    cubicValueLineChart.addSeries(series14);
                 break;
 
             case (R.id.d_30):
-                cubicValueLineChart.clearChart();
-                cubicValueLineChart.addSeries(series30);
+                currentSeries = "4";
+                if (series30.getSeries().size() == 0)
+                    new JSONTaskGraph().execute(highLowUrl30, "4");
+                else
+                    cubicValueLineChart.addSeries(series30);
                 break;
 
             case (R.id.d_60):
-                cubicValueLineChart.clearChart();
-                cubicValueLineChart.addSeries(series60);
+                currentSeries = "5";
+                if (series60.getSeries().size() == 0)
+                    new JSONTaskGraph().execute(highLowUrl60, "5");
+                else
+                    cubicValueLineChart.addSeries(series60);
                 break;
 
             case (R.id.d_90):
-                cubicValueLineChart.clearChart();
-                cubicValueLineChart.addSeries(series90);
+                currentSeries = "6";
+                if (series90.getSeries().size() == 0)
+                    new JSONTaskGraph().execute(highLowUrl90, "6");
+                else
+                    cubicValueLineChart.addSeries(series90);
                 break;
 
             case (R.id.alltime):
-                cubicValueLineChart.clearChart();
-                cubicValueLineChart.addSeries(seriesAll);
+                currentSeries = "7";
+                if (seriesAll.getSeries().size() == 0)
+                    new JSONTaskGraph().execute(highLowUrlAll, "7");
+                else
+                    cubicValueLineChart.addSeries(seriesAll);
                 break;
         }
     }
 
-    private void fillInfoFromJSONGraph() {
+    private void fillInfoFromJSONHighLow() {
         if (getView() != null) {
             highVal = getView().findViewById(R.id.high);
             highVal.setText(myGlobalsFunctions.commaSeperateInteger(high, true));
             lowVal = getView().findViewById(R.id.low);
             lowVal.setText(myGlobalsFunctions.commaSeperateInteger(low, true));
             cubicValueLineChart.addSeries(series24);
+        }
+    }
+
+    private void fillInfoFromJSONGraph() {
+        cubicValueLineChart.clearChart();
+        switch (currentSeries) {
+            case "1": cubicValueLineChart.addSeries(series24);
+                      break;
+
+            case "2": cubicValueLineChart.addSeries(series7);
+                      break;
+
+            case "3": cubicValueLineChart.addSeries(series14);
+                      break;
+
+            case "4": cubicValueLineChart.addSeries(series30);
+                      break;
+
+            case "5": cubicValueLineChart.addSeries(series60);
+                      break;
+
+            case "6": cubicValueLineChart.addSeries(series90);
+                      break;
+
+            case "7": cubicValueLineChart.addSeries(seriesAll);
+                      break;
         }
     }
 
@@ -200,8 +251,52 @@ public class StockInfoTab extends Fragment implements View.OnClickListener {
         }
     }
 
-    public void fillGraphValues(String highLowJson24, String highLowJson7, String highLowJson14, String highLowJson30,
-                                String highLowJson60, String highLowJson90, String highLowJsonAll) throws JSONException{
+    public void setValsGraph(String highLowJson) throws JSONException{
+
+        if (highLowJson!=null && !Objects.equals(highLowJson, "")) {
+            JSONObject highLowObj = new JSONObject(highLowJson);
+            JSONArray newRef = highLowObj.optJSONArray("stats");
+
+            switch (currentSeries) {
+                case "1" :  for (int i = 0; i < newRef.length(); ++i) {
+                                series24.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
+                            }
+                            break;
+
+                case "2" :  for (int i = 0; i < newRef.length(); ++i) {
+                                series7.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
+                            }
+                            break;
+
+                case "3" :  for (int i = 0; i < newRef.length(); ++i) {
+                                series14.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
+                            }
+                            break;
+
+                case "4" :  for (int i = 0; i < newRef.length(); ++i) {
+                                series30.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
+                            }
+                            break;
+
+                case "5" :  for (int i = 0; i < newRef.length(); ++i) {
+                                series60.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
+                            }
+                            break;
+
+                case "6" :  for (int i = 0; i < newRef.length(); ++i) {
+                                series90.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
+                            }
+                            break;
+
+                case "7" :  for (int i = 0; i < newRef.length(); ++i) {
+                                seriesAll.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
+                            }
+                            break;
+            }
+        }
+    }
+
+    public void setValsHighLow(String highLowJson24) throws JSONException {
         if (highLowJson24!=null && !Objects.equals(highLowJson24, "")) {
             JSONObject highLowObj = new JSONObject(highLowJson24);
             JSONArray newRef = highLowObj.optJSONArray("stats");
@@ -213,64 +308,10 @@ public class StockInfoTab extends Fragment implements View.OnClickListener {
             }
             high = Float.toString(max);
             low = Float.toString(min);
-
             for (int i = 0; i < newRef.length(); ++i) {
                 series24.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
             }
         }
-        if (highLowJson7!=null && !Objects.equals(highLowJson7, "")) {
-            JSONObject highLowObj = new JSONObject(highLowJson7);
-            JSONArray newRef = highLowObj.optJSONArray("stats");
-
-            for (int i = 0; i < newRef.length(); ++i) {
-                series7.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
-            }
-        }
-        if (highLowJson14!=null && !Objects.equals(highLowJson14, "")) {
-            JSONObject highLowObj = new JSONObject(highLowJson14);
-            JSONArray newRef = highLowObj.optJSONArray("stats");
-
-            for (int i = 0; i < newRef.length(); ++i) {
-                series14.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
-            }
-        }
-        if (highLowJson30!=null && !Objects.equals(highLowJson30, "")) {
-            JSONObject highLowObj = new JSONObject(highLowJson30);
-            JSONArray newRef = highLowObj.optJSONArray("stats");
-
-            for (int i = 0; i < newRef.length(); ++i) {
-                series30.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
-            }
-        }
-        if (highLowJson60!=null && !Objects.equals(highLowJson60, "")) {
-            JSONObject highLowObj = new JSONObject(highLowJson60);
-            JSONArray newRef = highLowObj.optJSONArray("stats");
-
-            for (int i = 0; i < newRef.length(); ++i) {
-                series60.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
-            }
-        }
-        if (highLowJson90!=null && !Objects.equals(highLowJson90, "")) {
-            JSONObject highLowObj = new JSONObject(highLowJson90);
-            JSONArray newRef = highLowObj.optJSONArray("stats");
-
-            for (int i = 0; i < newRef.length(); ++i) {
-                series90.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
-            }
-        }
-        if (highLowJsonAll!=null && !Objects.equals(highLowJsonAll, "")) {
-            JSONObject highLowObj = new JSONObject(highLowJsonAll);
-            JSONArray newRef = highLowObj.optJSONArray("stats");
-
-            for (int i = 0; i < newRef.length(); ++i) {
-                seriesAll.addPoint(new ValueLinePoint(myGlobalsFunctions.getEpochToNormalDateString(newRef.optJSONArray(i).optString(0)), Float.parseFloat(newRef.optJSONArray(i).optString(1))));
-            }
-        }
-    }
-
-    public void setValsGraph(String highLowJson24, String highLowJson7, String highLowJson14, String highLowJson30,
-                        String highLowJson60, String highLowJson90, String highLowJsonAll) throws JSONException {
-        fillGraphValues(highLowJson24, highLowJson7, highLowJson14, highLowJson30, highLowJson60, highLowJson90, highLowJsonAll);
     }
 
     public void setVals(String finalJson) throws JSONException {
@@ -330,7 +371,7 @@ public class StockInfoTab extends Fragment implements View.OnClickListener {
         }
     }
 
-    public class JSONTaskGraph extends AsyncTask<String,String, String > {
+    public class JSONTaskHighLow extends AsyncTask<String,String, String > {
 
         @Override
         protected void onPreExecute() {
@@ -341,16 +382,11 @@ public class StockInfoTab extends Fragment implements View.OnClickListener {
         protected String doInBackground(String... params) {
             try {
                 if (myGlobalsFunctions.isNetworkConnected()) {
-                    String highLowJson24 = myGlobalsFunctions.fetchJSONasString(params[0]);
-                    String highLowJson7 = myGlobalsFunctions.fetchJSONasString(params[1]);
-                    String highLowJson14 = myGlobalsFunctions.fetchJSONasString(params[2]);
-                    String highLowJson30 = myGlobalsFunctions.fetchJSONasString(params[3]);
-                    String highLowJson60 = myGlobalsFunctions.fetchJSONasString(params[4]);
-                    String highLowJson90 = myGlobalsFunctions.fetchJSONasString(params[5]);
-                    String highLowJsonAll = myGlobalsFunctions.fetchJSONasString(params[6]);
-                    if (highLowJson24 != null)
-                        setValsGraph(highLowJson24, highLowJson7, highLowJson14, highLowJson30, highLowJson60, highLowJson90, highLowJsonAll);
-                    return highLowJson24;
+                    String highLowJson = myGlobalsFunctions.fetchJSONasString(params[0]);
+                    if (highLowJson != null) {
+                        setValsHighLow(highLowJson);
+                    }
+                    return highLowJson;
                 }
 
             } catch (IOException | JSONException e) {
@@ -362,8 +398,49 @@ public class StockInfoTab extends Fragment implements View.OnClickListener {
         @Override
         protected void onPostExecute(String result) {
 //            super.onPostExecute(result);
-            if (result != null)
-                fillInfoFromJSONGraph();
+            if (result != null) {
+                fillInfoFromJSONHighLow();
+            }
+        }
+    }
+
+    public class JSONTaskGraph extends AsyncTask<String,String, String > {
+        String num;
+
+        @Override
+        protected void onPreExecute() {
+            super.onPreExecute();
+        }
+
+        @Override
+        protected String doInBackground(String... params) {
+            try {
+                if (myGlobalsFunctions.isNetworkConnected()) {
+                    String highLowJson = myGlobalsFunctions.fetchJSONasString(params[0]);
+                    num = params[1];
+                    return highLowJson;
+                }
+
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+            return  null;
+        }
+
+        @Override
+        protected void onPostExecute(String result) {
+//            super.onPostExecute(result);
+            try {
+                if (result != null) {
+                    if (num.equals(currentSeries)) {
+                        setValsGraph(result);
+                        fillInfoFromJSONGraph();
+                    }
+                }
+            }
+            catch (JSONException e) {
+                e.printStackTrace();
+            }
         }
     }
 }
