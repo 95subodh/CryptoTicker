1d5407b4afe5270abe4d951780c57379119f9281
Ankita1809 <ankitaverma@ANKITAs-MacBook.local>
2017-12-08 11:20:52 +0530

    scrolling issue myportfolio tab

--- app/src/main/java/com/apps/sky/cryptoticker/HomePage/HomePageTabs/MyPortfolioTab/MyPortfolioTab.java
+++ app/src/main/java/com/apps/sky/cryptoticker/HomePage/HomePageTabs/MyPortfolioTab/MyPortfolioTab.java
@@ -12,6 +12,7 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 import android.widget.TextView;
 
@@ -191,8 +192,8 @@ public class MyPortfolioTab extends Fragment implements SwipeRefreshLayout.OnRef
         }
         else {
             LayoutInflater inflater = (LayoutInflater) this.getActivity().getSystemService(Context.LAYOUT_INFLATER_SERVICE);
-            ((RelativeLayout) rootView.findViewById(R.id.main_view)).removeAllViews();
-            ((RelativeLayout) rootView.findViewById(R.id.main_view)).addView(inflater.inflate(R.layout.empty_view_layout, ((ViewGroup)getView().getParent()), false));
+            ((LinearLayout) rootView.findViewById(R.id.main_view)).removeAllViews();
+            ((LinearLayout) rootView.findViewById(R.id.main_view)).addView(inflater.inflate(R.layout.empty_view_layout, ((ViewGroup)getView().getParent()), false));
             ((TextView) rootView.findViewById(R.id.empty_view_text)).setText("Your portfolio is empty");
             rootView.refreshDrawableState();
         }
